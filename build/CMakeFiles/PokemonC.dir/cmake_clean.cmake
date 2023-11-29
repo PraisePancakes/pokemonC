3adfc696ade443a9a5d4827109eecd8e5f7cbd25
PraisePancakes <100659653+PraisePancakes@users.noreply.github.com>
2023-11-29 02:07:04 -0500

    fixed cmake bug :)

---
+++ build/CMakeFiles/PokemonC.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PokemonC.dir/src/main.c.obj"
+  "CMakeFiles/PokemonC.dir/src/main.c.obj.d"
+  "PokemonC.exe"
+  "PokemonC.exe.manifest"
+  "PokemonC.pdb"
+  "libPokemonC.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/PokemonC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/PokemonC.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for PokemonC.
+# This may be replaced when dependencies are built.
